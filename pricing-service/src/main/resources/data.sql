8ec7f7ef634f54974a599c4fc3c21c26e8afd189
rrbiz662 <rrbiz662@gmail.com>
2020-11-23 20:15:22 -0600

    Converted Pricing Service API into a microservice w/ Spring Data Rest

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO price (currency, price, vehicle_id) VALUES ('dollar', 55.00, 3);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('yen', 25.00, 2);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('peso', 10.00, 1);
